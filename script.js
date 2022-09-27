75a3d29209b8b013b1150c92dce4117d6e5b7e30
Surasit20 <84635760+Surasit20@users.noreply.github.com>
2022-09-27 16:59:00 +0700

    fix responsive

--- script.js
+++ script.js
@@ -45,23 +45,23 @@ window.addEventListener('resize', function () {
   if (window.innerWidth < 1400) {
     removeComponent();
     addFont();
-
-    if (window.innerWidth < 1200) {
-      addBackGround();
-    }
-    if (window.innerWidth < 991) {
-      addContentFPPT();
-    }
-
-    if (window.innerWidth >= 1200) {
-      removeBackGround();
-    }
-    if (window.innerWidth > 991) {
-      removeContentFPPT();
-    }
   } else {
     addComponent();
     removeFont();
+  }
+
+  if (window.innerWidth < 1200) {
+    addBackGround();
+  }
+  if (window.innerWidth < 991) {
+    addContentFPPT();
+  }
+
+  if (window.innerWidth >= 1200) {
+    removeBackGround();
+  }
+  if (window.innerWidth > 991) {
+    removeContentFPPT();
   }
 });
 
@@ -82,7 +82,6 @@ const addComponent = () => {
 };
 
 const addFont = () => {
-  //logic text
   let element1 = document.getElementById('text-context');
   element1.classList.add('content-text-header-2', 'text-center');
   element1.classList.remove('content-text-header-1');
@@ -92,6 +91,14 @@ const addFont = () => {
   for (let i = 0; i < bracket.length; i++) {
     bracket[i].innerHTML = '';
   }
+  // tab digital
+  let element2 = document.getElementById('digital');
+  element2.classList.add('d-block');
+
+  //logic text content
+  let element3 = document.getElementById('content-home');
+  element3.innerHTML =
+    ' พื้นที่จัดเก็บข้อมูล ให้กับแพลตฟอร์ม<br>หรือแอพพลิเคชั่น ตามความต้องการ <br>ของผู้ใช้งานกลุ่มเฉพาะ ปลดล็อคข้อจำกัดต่าง ๆ<br>ด้วยเทคโนโลยีบริหารที่มีความยืดหยุ่น รองรับการ<br>ใช้งาน  เพิ่มประสิทธิภาพการทำงานสูงสุดและความ<br>ปลอดภัยที่เชื่อมโยงผู้ใช้งานเข้าด้วยกันได้อย่าง<br>ไร้รอยต่อตลอด 24 ชม.';
 };
 
 const removeFont = () => {
@@ -104,13 +111,21 @@ const removeFont = () => {
   //add bracket
   bracket[0].innerHTML = '{';
   bracket[1].innerHTML = '}';
+
+  // tab digital
+  let element2 = document.getElementById('digital');
+  element2.classList.remove('d-block');
+
+  //logic text content
+  let element3 = document.getElementById('content-home');
+  element3.innerHTML =
+    ' พื้นที่จัดเก็บข้อมูล ให้กับแพลตฟอร์ม หรือแอพพลิเคชั่น <br> ตามความต้องการ ของผู้ใช้งานกลุ่มเฉพาะ ปลดล็อคข้อจำกัดต่าง ๆ<br>ด้วยเทคโนโลยีบริหารที่มีความยืดหยุ่น รองรับการใช้งาน<br>เพิ่มประสิทธิภาพการทำงานสูงสุดและความปลอดภัย<br>ที่เชื่อมโยงผู้ใช้งานเข้าด้วยกันได้อย่างไร้รอยต่อตลอด 24 ชม.';
 };
 
 const addBackGround = () => {
   //logic change background
   document.getElementById('bgId-3').src =
     './images/part-3-mobile/bg3-mobile.png';
-
   document.getElementById('bgId-1').src = './images/part-1/Group 1.png';
   let element1 = document.getElementById('bgId-3');
   let element2 = document.getElementById('part3mobile');
@@ -137,22 +152,27 @@ const addContentFPPT = () => {
   let element2 = document.getElementById('text-context-fppt-mobile');
   let element3 = document.getElementById('text-context-abbc-mobile');
   element1.innerHTML = '';
-  element3.classList.add('text-center', 'fs-6');
-  element2.classList.add('text-center', 'fs-6');
-  element3.innerHTML =
-    'ด้วยการนำ <b style="color:rgba(15, 26, 92, 1); font-family: Anantason-Medium; font-size: 1rem;">เทคโนโลยี ABBC (เอดับเบิ้ลบีซี)</b><br>เข้ามาบริหารจัดการธุรกิจให้เกิดการ Sharing Economic บน Eco system<br>ของตนเอง เพื่อสร้างสังคมแห่งการแบ่งปัน<br>พร้อมทั้งต่อยอดธุรกิจในหลากหลายแพลตฟอร์ม ยกระดับธุรกิจให้เติบโต<br>และสร้างรากฐานที่มั่นคง เตรียมศักยภาพความพร้อมในการแข่งขัน<br>ที่เพิ่มขึ้นในอนาคต';
+  element2.classList.add('text-center', 'content-text');
+  element3.classList.add('text-center');
+  element2.style.fontSize = '0.75rem';
+  element3.style.fontSize = '0.75rem';
+  element2.classList.remove('fs-5');
+  element3.classList.remove('fs-5');
   element2.innerHTML =
-    ' คอร์ แอสเซท มุ่งเน้นบริหารจัดการดิจิตอลแอสเซท<br>   <b style="color:rgba(15, 26, 92, 1); font-family: Anantason-Medium; font-size: 1rem;">สร้างระบบด้วยแนวคิด FPPT (เอฟ ดับเบิ้ลพี ที)</b><br>เพื่อจัดการบริหารธุรกิจและส่งเสริมการสร้างผลิตภัณฑ์<br>ในเครือของบริษัทคอร์ แอสเซท';
-  element2.classList.add('text-center');
+    'คอร์ แอสเซท มุ่งเน้นบริหารจัดการดิจิตอลแอสเซท<br> <b style="color:rgba(15, 26, 92, 1); font-family: Anantason-Medium; font-size: 0.8rem;">สร้างระบบด้วยแนวคิด FPPT (เอฟ ดับเบิ้ลพี ที)</b><br>เพื่อจัดการบริหารธุรกิจและส่งเสริมการสร้างผลิตภัณฑ์<br>ในเครือของบริษัทคอร์ แอสเซท';
+  element3.innerHTML =
+    'ด้วยการนำ <b style="color:rgba(15, 26, 92, 1); font-family: Anantason-Medium; font-size: 0.8rem;">เทคโนโลยี ABBC<br>(เอดับเบิ้ลบีซี)</b>เข้ามาบริหารจัดการธุรกิจ<br>ให้เกิดการ Sharing Economic บน Eco system ของตนเอง เพื่อสร้างสังคม<br>แห่งการแบ่งปันพร้อมทั้งต่อยอดธุรกิจ<br>ในหลากหลายแพลตฟอร์ม ยกระดับธุรกิจให้เติบโต<br>และสร้างรากฐานที่มั่นคง เตรียมศักยภาพความพร้อม<br>ในการแข่งขันที่เพิ่มขึ้นในอนาคต';
 };
 
 const removeContentFPPT = () => {
   let element1 = document.getElementById('text-context-fppt-wide');
   let element2 = document.getElementById('text-context-fppt-mobile');
   let element3 = document.getElementById('text-context-abbc-mobile');
-  element3.classList.remove('text-center', 'fs-6');
-  element2.classList.remove('text-center', 'fs-6');
+  element1.classList.remove('text-center');
+  element2.classList.remove('text-center', 'content-text');
   element3.classList.remove('text-center');
+  element2.classList.add('fs-5');
+  element3.classList.add('fs-5');
   element2.innerHTML = '';
   element1.innerHTML =
     ' คอร์ แอสเซท มุ่งเน้นบริหารจัดการดิจิตอลแอสเซท<br>   <b style="color:rgba(15, 26, 92, 1); font-family: Anantason-Medium; font-size: 1.5rem;">สร้างระบบด้วยแนวคิด FPPT (เอฟ ดับเบิ้ลพี ที)</b><br>เพื่อจัดการบริหารธุรกิจและส่งเสริมการสร้างผลิตภัณฑ์<br>ในเครือของบริษัทคอร์ แอสเซท';
